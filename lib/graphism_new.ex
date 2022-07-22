04eca4e163d6049853cd6cac193a924f1b16c7dd
Pedro Gutiérrez <pedrogutierrez@mac.com>
2022-07-22 23:11:35 +0200

    Initial commit

---
+++ lib/graphism_new.ex
@@ -0,0 +1,18 @@
+defmodule GraphismNew do
+  @moduledoc """
+  Documentation for `GraphismNew`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> GraphismNew.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/graphism_new_test.exs
@@ -0,0 +1,8 @@
+defmodule GraphismNewTest do
+  use ExUnit.Case
+  doctest GraphismNew
+
+  test "greets the world" do
+    assert GraphismNew.hello() == :world
+  end
+end
