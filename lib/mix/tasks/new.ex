5920c5031d7c1db173ce5ee1bb2a4bfe18b33d76
Pedro Gutiérrez <pedrogutierrez@mac.com>
2022-08-07 18:18:54 +0200

    Upgrade to Graphism v0.8.0

--- lib/mix/tasks/new.ex
+++ lib/mix/tasks/new.ex
@@ -296,7 +296,7 @@ defmodule Mix.Tasks.Graphism.New do
     # Run "mix help deps" to learn about dependencies.
     defp deps do
       [
-        {:graphism, git: "https://github.com/gravity-core/graphism.git", branch: "rest"}
+        {:graphism, git: "https://github.com/gravity-core/graphism.git", tag: "v0.8.0"}
       ]
     end
   end
